ffc2db0d75bc2429f9900a9ed030f02de7fdde51
Avinashbindapi <112742695+Avinashbindapi@users.noreply.github.com>
2023-04-17 15:50:51 +0530

    init

---
+++ app/config/confg.php
@@ -0,0 +1,14 @@
+<?php
+$host = 'localhost';
+$user = 'root';
+$password = '';
+$dbname = 'dugdham';
+
+try {
+    $conn = new PDO("mysql:host=$host;dbname=$dbname", $user, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    CommonHelper::setDb($conn);
+} catch(PDOException $e) {
+    echo 'Connection failed: ' . $e->getMessage();
+}
+?>
